28a3a5a6d04a50df723dc51b98f132d08146f5f6
alecalve <antoine@alc.io>
2013-03-22 21:48:13 +0100

    Ajout de la page évènements

---
+++ evenements.php
@@ -0,0 +1,13 @@
+<?php
+include('head.php');
+?>
+        <div class="hero-unit">
+            <h3>Évènements</h3>
+            <p>À venir</p>
+        </div>
+        <script>
+            $("#evenements").addClass("active");
+        </script>
+<?php
+include('tail.php');
+?>
