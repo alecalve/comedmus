9692599866f01a6fb120ccbe8521d7bb7f74583f
alecalve <antoine@alc.io>
2013-03-22 21:50:32 +0100

    Suppression du vrai synopsis

--- synopsis.php
+++ synopsis.php
@@ -3,7 +3,7 @@ include('head.php');
 ?>
         <div class="hero-unit">
             <h3>Mais qui est donc ce caméléon ?</h3>
-            <p>Claude, jeune détective privée de trente ans, se réveille un matin avec un prénom en tête. Un prénom qui la suit partout : celui de Camille. Un curieux client lui demande de partir à la recherche du détenteur d’une mallette, portant le même nom.</p><p>De plus en plus intriguée, Claude se lance à la poursuite de cette mystérieuse « personne », dans des lieux divers, des plus communs (supermarché, avion, simple appartement) aux plus exotiques (Afrique, service militaire, etc).</p><p>Au cours de ces aventures, Claude risque de retrouver bien plus que ce qu’elle cherchait…<p>
+            <p>Patience, vous saurez tout en temps voulu.</p>
         </div>
         <script>
             $("#synopsis").addClass("active");
