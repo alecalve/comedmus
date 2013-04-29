3f509a8258d74c649820c1ee7fa3e75b1b3a4f6e
Antoine <antoine@DaPC.(none)>
2013-04-29 18:04:02 +0200

    Ajout de la liste des partenaires

--- head.php
+++ head.php
@@ -7,6 +7,7 @@
     <link href="style.css" rel="stylesheet" type="text/css">
     <link rel="shortcut icon" href="favicon.ico"/>
     <script src="static/jquery.js"></script>
+    <script src="static/bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
     <!-- Menu -->
@@ -18,7 +19,13 @@
                     <li id="presentation"><a href="presentation.php">Qu'est-ce-que la Com' ?</a></li>
                     <li id="synopsis"><a href="synopsis.php">Synopsis</a></li>
                     <li id="evenements"><a href="evenements.php">Évènements</a></li>
-                    <li id="partenaires"><a href="partenaires.php">Partenaires</a></li>
+                    <li class="dropdown">
+                      <a id="partenaires" href="#" role="button" class="dropdown-toggle" data-toggle="dropdown">Partenaires <b class="caret"></b></a>
+                      <ul class="dropdown-menu" role="menu" aria-labelledby="Partenaires">
+                        <li><a tabindex="-1" href="partenaires.php">Devenir partenaire</a></li>
+                        <li><a tabindex="-1" href="partenaires_liste.php">Nos partenaires</a></li>
+                      </ul>
+                    </li>
                     <li id="contact"><a href="contact.php">Contact</a></li>
                 </ul>
             </div>
---
+++ partenaires_liste.php
@@ -0,0 +1,53 @@
+<?php
+include('head.php');
+?>
+		<div class="row">
+			<div class="span6">
+				<ul class="thumbnails">
+					<li class="span6">
+						<div class="thumbnail text-center">
+							<img src="media/compiegne_musique.jpg" alt="Compiègne Musique">
+							<h4>Compiegne Musique</h4>
+						</div>
+					</li>
+					<li class="span6">
+						<div class="thumbnail text-center">
+							<img src="media/brulerie_cour_le_roi.jpg" alt="Brulerie cour le roi">
+							<h4>Brulerie cour le roi</h4>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<div class="span6">
+				<ul class="thumbnails">
+					<li class="span6">
+						<div class="thumbnail text-center">
+							<img src="media/ultima_games.jpg" alt="Ultima Games">
+							<h4>Ultima Games</h4>
+							<p>Ouvert depuis décembre 1996, en plein coeur de Compiègne, Ultima te propose un large choix de jeux vidéos neufs ou d'occassions sur consoles et micro ordinateurs.</p>
+							<p>10, rue Bonnetiers - 60200 Compiègne</p>
+							<p>Tél : 03 44 40 48 97</p>
+						</div>
+					</li>
+					<li class="span6">
+						<div class="thumbnail text-center">
+							<img src="media/conduite2000.png" alt="Conduite 2000">
+							<h4>Conduite 2000</h4>
+						</div>
+					</li>
+					<li class="span6">
+						<div class="thumbnail text-center">
+							<img src="media/voyvoy.gif" alt="Voy Voy">
+							<h4>Voy Voy</h4>
+						</div>
+					</li>
+				</ul>
+			</div>
+		
+		</div>
+        <script>
+            $("#partenaires").addClass("active");
+        </script>
+<?php
+include('tail.php');
+?>
