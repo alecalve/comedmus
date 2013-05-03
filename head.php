ee4b1d623acf5a305dc824dea32d0aed3441425f
Samuel Lepetit <lepetit.samuel@gmail.com>
2013-05-03 13:34:36 +0200

    Modified index.php for the new structure

--- head.php
+++ head.php
@@ -10,27 +10,8 @@
     <script src="static/bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
-    <!-- Menu -->
-    <div class="navbar navbar-fixed-top">
-        <div class="navbar-inner">
-            <div class="container">
-                <ul class="nav">
-                    <li id="index"><a href="index.php"> Accueil</a></li>
-                    <li id="presentation"><a href="presentation.php">Qu'est-ce-que la Com' ?</a></li>
-                    <li id="synopsis"><a href="synopsis.php">Synopsis</a></li>
-                    <li id="evenements"><a href="evenements.php">Évènements</a></li>
-                    <li class="dropdown">
-                      <a id="partenaires" href="#" role="button" class="dropdown-toggle" data-toggle="dropdown">Partenaires <b class="caret"></b></a>
-                      <ul class="dropdown-menu" role="menu" aria-labelledby="Partenaires">
-                        <li><a tabindex="-1" href="partenaires.php">Devenir partenaire</a></li>
-                        <li><a tabindex="-1" href="partenaires_liste.php">Nos partenaires</a></li>
-                      </ul>
-                    </li>
-                    <li id="contact"><a href="contact.php">Contact</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    <!-- Fin menu -->
     <!-- Main content -->
-    <div class="container">
+    <div class="container"> 
+
+
+
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+  //Inclusion du header de la page
+  include('head.php');
+
+  
+  //Inclusion du pied de page
+  include('tail.php');
+?>
