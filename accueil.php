657521129b885f659590654a159ac494695644e7
Antoine <antoine@DaPC.(none)>
2013-05-03 14:10:29 +0200

    Ajout du splash d'accueil

---
+++ accueil.php
@@ -0,0 +1,3 @@
+<div class="index text-center">
+	<img class="logo" src="static/logo.svg" alt="Logo">
+</div>
--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
   //Inclusion du header de la page
   include('head.php');
+  
+  include('accueil.php');
     
   //Récupération de la liste des pages de données
   include('data.php');
-  
+    
   //Inclusion du pied de page
   include('tail.php');
 ?>
