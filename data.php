af2f648a3bc43a5db8e4238d570792662ea84fbc
Samuel Lepetit <lepetit.samuel@gmail.com>
2013-05-03 13:52:10 +0200

    First straight implementation of data.php to load all the pages data when the index is loaded

--- data.php
+++ data.php
@@ -1,3 +1,12 @@
 <?php
-//TODO
+foreach (glob('data/*.php') as $filename) {
+  //TODO: Could be done in a cleaner way.
+  $moduleName = str_replace('data/','',$filename);
+  $moduleName = str_replace('.php','',$moduleName);
+  
+  echo '<div id="' . $moduleName . '" style="visibility:hidden;">' . "\n";
+  include($filename);
+  echo "\n" . '</div>' . "\n";
+    
+}
 ?>
