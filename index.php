fd199db1e261b73127d0c44cfab51468023868fe
Samuel Lepetit <lepetit.samuel@gmail.com>
2013-05-03 13:38:44 +0200

    Add data.php in index

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
   //Inclusion du header de la page
   include('head.php');
-
+    
+  //Récupération de la liste des pages de données
+  include('data.php');
   
   //Inclusion du pied de page
   include('tail.php');
