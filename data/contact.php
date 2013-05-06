9a99c2bacc76d9a070d0b0369d57553092d35c30
Samuel Lepetit <lepetit.samuel@gmail.com>
2013-05-06 12:09:21 +0200

    Add return button

--- data/contact.php
+++ data/contact.php
@@ -1,5 +1,5 @@
     <div class="hero-unit">
-      <a class="previous_link" href="#" onclick="hide_page('contact')"> <- </a>
+      <a class="previous_link" href="#" onclick="hide_page('contact')"><img src="static/retour.png" alt="<-" /></a>
         <p>Par mail à: <a href="mailto:comedmus@gmail.com">comedmus@gmail.com</a></p>
 
         <p>Par courrier à :</p>
